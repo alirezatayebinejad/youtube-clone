532b03ebe9bc8ef150aa9f23640c08f089d0ac1e
vibracode <codevibra@gmail.com>
2022-08-31 20:17:48 +0430

    videoCard component and responsived

--- src/components/Recomended/Recomended.js
+++ src/components/Recomended/Recomended.js
@@ -1,72 +1,107 @@
 import "./Recomended.css";
 import React from "react";
+import VideoCard from "../VideoCard/VideoCard";
 
 function Recomended() {
+	const videData = [
+		{
+			thumbnailSrc: "https://pbs.twimg.com/media/FE_X3qZXIAoSbgr?format=jpg&name=large",
+			title: "$456,000 Squid Game In Real Life",
+			logoSrc: "../../images/channel1.jpg",
+			channelName: "Mr Beast",
+			views: "54M views",
+			date: "3 years ago",
+			isVerified: true,
+		},
+		{
+			thumbnailSrc: "https://pbs.twimg.com/media/FE_X3qZXIAoSbgr?format=jpg&name=large",
+			title: "$456,000 Squid Game In Real Life",
+			logoSrc: "../../images/channel1.jpg",
+			channelName: "Mr Beast",
+			views: "54M views",
+			date: "3 years ago",
+			isVerified: true,
+		},
+		{
+			thumbnailSrc: "https://pbs.twimg.com/media/FE_X3qZXIAoSbgr?format=jpg&name=large",
+			title: "$456,000 Squid Game In Real Life",
+			logoSrc: "../../images/channel1.jpg",
+			channelName: "Mr Beast",
+			views: "54M views",
+			date: "3 years ago",
+			isVerified: true,
+		},
+		{
+			thumbnailSrc: "https://pbs.twimg.com/media/FE_X3qZXIAoSbgr?format=jpg&name=large",
+			title: "$456,000 Squid Game In Real Life",
+			logoSrc: "../../images/channel1.jpg",
+			channelName: "Mr Beast",
+			views: "54M views",
+			date: "3 years ago",
+			isVerified: true,
+		},
+		{
+			thumbnailSrc: "https://pbs.twimg.com/media/FE_X3qZXIAoSbgr?format=jpg&name=large",
+			title: "$456,000 Squid Game In Real Life",
+			logoSrc: "../../images/channel1.jpg",
+			channelName: "Mr Beast",
+			views: "54M views",
+			date: "3 years ago",
+			isVerified: true,
+		},
+		{
+			thumbnailSrc: "https://pbs.twimg.com/media/FE_X3qZXIAoSbgr?format=jpg&name=large",
+			title: "$456,000 Squid Game In Real Life",
+			logoSrc: "../../images/channel1.jpg",
+			channelName: "Mr Beast",
+			views: "54M views",
+			date: "3 years ago",
+			isVerified: true,
+		},
+		{
+			thumbnailSrc: "https://pbs.twimg.com/media/FE_X3qZXIAoSbgr?format=jpg&name=large",
+			title: "$456,000 Squid Game In Real Life",
+			logoSrc: "../../images/channel1.jpg",
+			channelName: "Mr Beast",
+			views: "54M views",
+			date: "3 years ago",
+			isVerified: true,
+		},
+		{
+			thumbnailSrc: "https://pbs.twimg.com/media/FE_X3qZXIAoSbgr?format=jpg&name=large",
+			title: "$456,000 Squid Game In Real Life",
+			logoSrc: "../../images/channel1.jpg",
+			channelName: "Mr Beast",
+			views: "54M views",
+			date: "3 years ago",
+			isVerified: true,
+		},
+		{
+			thumbnailSrc: "https://pbs.twimg.com/media/FE_X3qZXIAoSbgr?format=jpg&name=large",
+			title: "$456,000 Squid Game In Real Life",
+			logoSrc: "../../images/channel1.jpg",
+			channelName: "Mr Beast",
+			views: "54M views",
+			date: "3 years ago",
+			isVerified: true,
+		},
+		{
+			thumbnailSrc: "https://pbs.twimg.com/media/FE_X3qZXIAoSbgr?format=jpg&name=large",
+			title: "$456,000 Squid Game In Real Life",
+			logoSrc: "../../images/channel1.jpg",
+			channelName: "Mr Beast",
+			views: "54M views",
+			date: "3 years ago",
+			isVerified: true,
+		},
+	];
 	return (
 		<div className="recomended">
-			Recomended
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
-			<p>recsldjfd</p>
+			<div className="recommended__videocards">
+				{videData.map((data) => (
+					<VideoCard {...data} />
+				))}
+			</div>
 		</div>
 	);
 }
---
+++ src/components/VideoCard/VideoCard.js
@@ -0,0 +1,29 @@
+import "./VideoCard.css";
+import React from "react";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { Avatar } from "@mui/material";
+
+function VideoCard({ thumbnailSrc, title, logoSrc, channelName, views, date, isVerified }) {
+	return (
+		<div className="videocard">
+			<img src={thumbnailSrc} alt="" className="videocard__thumbnail" />
+			<div className="videocard__desc">
+				<div className="videocard__logo">
+					<Avatar src={logoSrc} />
+				</div>
+				<div>
+					<p className="videocard__title">{title}</p>
+					<p className="videocard__channelname">
+						{channelName}
+						{isVerified && <CheckCircleIcon sx={{ fontSize: 18 }} />}
+					</p>
+					<p className="videocard__data">
+						{views} <b>.</b> {date}
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default VideoCard;
